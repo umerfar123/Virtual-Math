450818a86d419fab02f7d5c647f3e65b693a5bd6
umer_far <farooqueumer2662@gmail.com>
2024-09-05 23:19:11 +0530

    initial implementation

---
+++ api.py
@@ -0,0 +1,1 @@
+apk= 'AIzaSyBftyAMQwKRgyt7aBfTbUJfxAoWHUyDYiY'
--- gestureMath.py
+++ gestureMath.py
@@ -1,6 +1,28 @@
 import cv2
 import handtracking as ht
 import numpy  as np
+from PIL import Image
+import os
+from api import apk
+import streamlit as st
+
+import google.generativeai as genai
+genai.configure(api_key=apk)
+model = genai.GenerativeModel(model_name="gemini-1.5-flash")
+
+st.set_page_config(layout= 'wide')
+st.image('D:\Projects\Opencv\mathgesture\GestureMath.png')
+col1,col2 = st.columns([1,2])
+
+
+with col2:
+    st.checkbox('Run',value=True)
+    col2_window = st.image([])
+    
+with col1:
+    st.title("Answers:")
+    col1_window = st.subheader(" ")
+    
 
 hand = ht.handDetector()
 cap = cv2.VideoCapture(0)
@@ -13,7 +35,6 @@ def handdet(frame):
     
     if len(lmList)!=0 :
         fingers = hand.fingersUp(lmList)
-        print(fingers)
         
     return frame,fingers,lmList
 
@@ -28,21 +49,30 @@ def draw(canvas,fingers,lmList,oldpoint):
         cv2.line(canvas,pt1=oldpoint,pt2=currentpoint,color=(0,255,255),thickness=10)
         oldpoint = currentpoint
         
-    elif fingers == [0,0,0,0,0]:
+    elif fingers == [1,0,0,0,0]:
         
         canvas = np.zeros_like(frame)
               
     return canvas
-def senttoAi(frame):
+
+def senttoAi(frm,fingers):
+    
+    if fingers == [0,1,0,0,1]:
+        PIL_image = Image.fromarray(frm)
+        response = model.generate_content(["solve this math problem",PIL_image])
+        return response.text
     
 
 oldpoint= None
 canvas = None
 currentpoint = None
+aioutput = None
 
 while True:
     
     status, frame = cap.read()
+    frame = cv2.resize(frame, (720,400))
+    frame = cv2.flip(frame , 1)
     
     if not (status):
         print("Capturing error")
@@ -50,20 +80,19 @@ while True:
     if canvas is None:
         canvas = np.zeros_like(frame)
     
-    frame = cv2.resize(frame, (720,400))
-    frame = cv2.flip(frame , 1)
-    
     frame,fingers,lmList = handdet(frame)
     
     if len(lmList)!=0:
-        
         canvas = draw(canvas,fingers,lmList,oldpoint)
-    
-   
+        aioutput = senttoAi(canvas,fingers)
         
+    frame_combined = cv2.addWeighted(frame,0.7, canvas,0.3,1)
     
-    cv2.imshow('Gesture Math',frame)
-    cv2.imshow('canvas',canvas)
+    col2_window.image(frame_combined, channels='BGR')
+    if aioutput:
+        col1_window.text(aioutput)
+    #cv2.imshow('Gesture Math',frame_combined)
+    #xcv2.imshow('canvas',canvas)
     
     if cv2.waitKey(1) & 0xFF == ord('x'):
         break
